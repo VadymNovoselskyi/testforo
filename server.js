8800199ee649beea98c0284a932da3f89de07d00
VadymNovoselskyi <novoseslkyi.vadim@gmail.com>
2021-12-02 13:42:01 +0100

    debug

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.static('public'));
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
 // Port website will run on
-app.listen(5050);
-/* app.listen(process.env.PORT); */
+/* app.listen(5050); */
+app.listen(process.env.PORT);
 // *** GET Routes - display pages ***
 // Root Route
 app.get('/', function (req, res) {
